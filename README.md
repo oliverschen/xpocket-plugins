5f6ec845fb2eb7f31afa5ba883a642e5b64ce4d8
chenkui <jihe12180@gmail.com>
2022-05-02 18:05:49 +0800

    print log

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# xpocket-plugins
-xpocket 平台插件集合
+# XPocket-plugins
+XPocket 平台插件集合
 
 XPocket 是PerfMa为终结性能问题而生的开源插件容器，这是 Xpocket 平台的官方介绍，[Github 地址](https://github.com/PerfMa/XPocket)
--- xpocket-plugin-jvm/src/main/java/com/perfma/xlab/xpocket/plugin/jvm/JvmXPocketPlugin.java
+++ xpocket-plugin-jvm/src/main/java/com/perfma/xlab/xpocket/plugin/jvm/JvmXPocketPlugin.java
@@ -1,7 +1,18 @@
 package com.perfma.xlab.xpocket.plugin.jvm;
 
 import com.perfma.xlab.xpocket.spi.AbstractXPocketPlugin;
+import com.perfma.xlab.xpocket.spi.process.XPocketProcess;
 
 public class JvmXPocketPlugin extends AbstractXPocketPlugin {
+    private static final String LOG = "________                   \n" +
+            "______(_)__   ________ ___ \n" +
+            "_____  /__ | / /_  __ `__ \\\n" +
+            "____  / __ |/ /_  / / / / /\n" +
+            "___  /  _____/ /_/ /_/ /_/ \n" +
+            "/___/                      ";
 
+    @Override
+    public void printLogo(XPocketProcess process) {
+        process.output(LOG);
+    }
 }
